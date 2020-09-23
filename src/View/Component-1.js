e0d1cab6e7bacc2059280bd4916c86ec6b27d473
Ömer Kaya <f.omerkaya@outlook.com>
2020-09-23 17:31:27 -0400

    Lesson-12

--- src/View/Component-1.js
+++ src/View/Component-1.js
@@ -1,5 +1,47 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-let txt = (s) => React.createElement('h1',{className:'container'},s);
+class Counter extends React.Component
+{
+    state = 
+    {
+        i: 0
+    }
 
-export default txt;
+    inc = () =>
+    {
+        this.setState({i: this.state.i+1})
+    }
+
+    dec = () =>
+    {
+        this.setState({i: this.state.i-1})
+    }
+
+    render()
+    {
+        
+        var incButton = React.createElement('button',{onClick:this.inc},'increase');
+        var decButton = React.createElement('button',{onClick:this.dec},'decrease');
+        var container = React.createElement('div',{className:'container'}, incButton, decButton);
+        var label = React.createElement('label',{}, this.state.i);
+        return React.createElement('div',{className:'container'},'Counter : ', label , container);
+        
+        /*return (
+            <div class="container">
+                <p>
+                    Counter :
+                </p>
+                <label>
+                    {this.state.i}
+                </label>
+                <div>
+                    <button onClick={this.inc} > Increase </button>
+                    <button onClick={this.dec} > Decrease </button>
+                </div>
+            </div>
+       );*/
+    }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
-import txt from './View/Component-1'
+import Counter from './View/Component-1'
 
+let txt = (s) => React.createElement('h1',{className:'container'},s);
 let txt1 = {className:'txt-Intro'};
 let helloworld = txt('hello world');
+
+let paragrahs = new Counter();
 
 ReactDOM.render
 (
     <React.StrictMode>
         {React.createElement('div',txt1,helloworld)}
+        <Counter />
     </React.StrictMode>,
     document.getElementById('root')
 )
